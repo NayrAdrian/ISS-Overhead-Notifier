02f75691e94d6af8919aaed3a7352096f9089f8e
Ryan Adrian <NayrAdrian24@gmail.com>
2024-07-17 18:06:28 +0800

    Added function to check if MY_LAT and MY_LONG are within -5 and +5 of ISS coordinates.

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 14.612034
-MY_LONG = 121.086786
+# MY_LAT = 14.612034
+# MY_LONG = 121.086786
+
+MY_LAT = -33.612034  # For testing purposes only +5 -5
+MY_LONG = -69.859  # For testing purposes only +5 -5
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
-data = response.json()
+iss_data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+iss_latitude = float(iss_data["iss_position"]["latitude"])
+iss_longitude = float(iss_data["iss_position"]["longitude"])
+my_latitude = float(MY_LAT)
+my_longitude = float(MY_LONG)
 
-# Your position is within +5 or -5 degrees of the ISS position.
+if (iss_latitude - 5 <= my_latitude <= iss_latitude + 5) and (iss_longitude - 5 <= my_longitude <= iss_longitude + 5):
+    print("Your position is within +5 or -5 degrees of the ISS latitude and longitude.")
 
+print(f"ISS Latitude: {iss_latitude}")
+print(f"ISS Longitude: {iss_longitude}")
+print(f"My Latitude: {my_latitude}")
+print(f"My Longitude: {my_longitude}")
 
 parameters = {
     "lat": MY_LAT,
@@ -28,10 +38,12 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
+print(sunrise)
+print(sunset)
+print(time_now)
+print(iss_data)
+
 # If the ISS is close to my current position
 # And it is currently dark
 # Then send an email to look up.
 # BONUS: run the code every 60 seconds.
-
-
-
